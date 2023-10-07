5e62eac14e3508b4af2be27f7636a17a213c3a7c
suzukiplan <suzukiplan.ys@gmail.com>
2023-10-08 07:16:54 +0900

    clang-format

--- example/rgb/rgb.cpp
+++ example/rgb/rgb.cpp
@@ -7,12 +7,12 @@ extern "C" void vgs_setup()
     vgs.gfx.startWrite();
     for (int i = 0; i < 32; i++) {
         int y = 0;
-        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, i << 11); // Red
-        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, i << 6 | 0x0020); // Green
-        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, i); // Blue
-        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, (i << 11) + i); // Red + Blue
-        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, (i << 6 | 0x0020) + i); // Green + Blue
-        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, (i << 11) + (i << 6 | 0x0020)); // Red + Green
+        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, i << 11);                           // Red
+        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, i << 6 | 0x0020);                   // Green
+        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, i);                                 // Blue
+        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, (i << 11) + i);                     // Red + Blue
+        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, (i << 6 | 0x0020) + i);             // Green + Blue
+        vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, (i << 11) + (i << 6 | 0x0020));     // Red + Green
         vgs.gfx.boxf(8 + i * 6, 8 + 8 * y++, 5, 5, (i << 11) + (i << 6 | 0x0020) + i); // Red + Green + Blue
     }
     vgs.gfx.endWrite();
